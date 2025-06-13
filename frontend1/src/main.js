2d8f98f90fd5ab78a5ee70413cd2e9fc5361b014
StefanyGHA <U20230748@univo.edu.sv>
2025-06-13 11:40:26 -0600

    Agregando imagenes, y estilo

--- frontend1/src/main.js
+++ frontend1/src/main.js
@@ -4,4 +4,12 @@ import './assets/styles.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App)
+
+app.mount('#app')
+
+
+
+
